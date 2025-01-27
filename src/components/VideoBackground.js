2f2fad24b84a9721381583d33072b16386405315
mvksim95 <maxime.tyson@gmail.com>
2025-01-27 20:03:24 +0100

    modif chemin video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import fumeeVideo from '../assets/videos/fumee.mp4' // chemin correct
 import '../styles/VideoBackground.sass';
 
 const VideoBackground = () => {
   return (
     <div className='video-section'>
       <video autoPlay loop muted playsInline className='background-video'>
-        <source src={fumeeVideo} type="video/mp4" />
+        <source src="/videos/fumee.mp4" type="video/mp4" />
         Votre navigateur ne supporte pas la lecture de vidéos.
       </video>
     </div>
